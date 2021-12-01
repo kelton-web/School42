1bc958bca5cce2339ae143291f559ceb076d09a7
Kelton Semedo da veiga <ksemedo-@c2r8s6.42lausanne.ch>
2021-12-01 15:37:33 +0100

    mise a jour avec printf

---
+++ Prinft42/ft_prinft_utils.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_prinft_utils.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksemedo- <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/01 09:46:58 by ksemedo-          #+#    #+#             */
+/*   Updated: 2021/12/01 15:33:32 by ksemedo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __FT_PRINFT__
+# define __FT_PRINFT__
+
+# include <stdarg.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+char ft_putstr(char *str);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ Prinft42/ft_printf.c
@@ -0,0 +1,123 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksemedo- <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/26 13:53:30 by ksemedo-          #+#    #+#             */
+/*   Updated: 2021/12/01 15:33:52 by ksemedo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_prinft_utils.h"
+
+int	ft_putchar(int c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	ft_format_find(va_list args, char tab)
+{
+	int	compteur;
+
+	compteur = 0;
+	if (tab == 'c')
+		compteur +=	ft_putchar('c');
+	if (tab == 's')
+		compteur +=	ft_putstr(va_list(args), tab);
+	if (tab == 'p')
+		compteur +=	ft_putchar('p');
+	if (tab == 'd')
+		compteur +=	ft_putchar('d');
+	if (tab == 'i')
+		compteur +=	ft_putchar('i');
+	if (tab == 'u')
+		compteur +=	ft_putchar('u');
+	if (tab == 'x')
+		compteur +=	ft_putchar('x');
+	if (tab == '%')
+		compteur += ft_putchar('%');
+	return (compteur);
+}
+
+int	ft_printf(const char *tab, ...)
+{
+	int		i;
+	va_list	args;
+	int		compteur;
+
+	va_start(args, tab);
+	i = 0;
+	compteur = 0;
+	while (tab[i])
+	{
+		if (tab[i] == '%')
+		{
+			compteur += ft_format_find(args, tab[i + 1]);
+			i++;
+		}
+		else
+		{
+			compteur += ft_putchar(tab[i]);
+		}
+		i++;
+	}
+	va_end(args);
+	return (compteur);
+}
+
+int main()
+{
+	char *str = "LOOL";
+	int adrss = 167;
+	unsigned int unbr = 354687524;
+	char c = '%';
+
+
+	printf("==========================\n");
+	printf("test avec des char\n");
+	printf("==========================\n");
+
+	printf("ft nbr: %d\n",ft_printf("%c\n", c));
+	printf("sy nbr: %d\n",printf("%c\n", c));
+
+
+	printf("==========================\n");
+	printf("test avec des string\n");
+	printf("==========================\n");
+
+	printf("ft nbr: %d\n",ft_printf("%s\n", str));
+	printf("sy nbr: %d\n",printf("%s\n", str));
+
+	printf("==========================\n");
+	printf("test avec des Int\n");
+	printf("==========================\n");
+
+	printf("ft nbr : %d\n",ft_printf("%d\n", adrss));
+	printf("sy nbr: %d\n",printf("%d\n", adrss));
+
+	printf("==========================\n");
+	printf("test avec des Unsigned Int\n");
+	printf("==========================\n");
+
+	printf("ft nbr: %d\n",ft_printf("%u\n", unbr));
+	printf("sy nbr: %d\n",printf("%u\n", unbr));
+
+
+	printf("==========================\n");
+	printf("test avec des Hexadecimal\n");
+	printf("==========================\n");
+
+	printf("ft nbr: %d\n",ft_printf("%x\n", adrss));
+	printf("sy nbr: %d\n",printf("%x\n", adrss));
+
+	printf("==========================\n");
+	printf("test avec des Adresse\n");
+	printf("==========================\n");
+
+	printf("ft nbr: %d\n",ft_printf("%p\n", &adrss));
+	printf("sy nbr: %d\n",printf("%p\n", &adrss));
+
+}
